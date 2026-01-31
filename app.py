d4f5ca23faba62345fc8c6958f435e8929e850ad
cordegangsta-eng <cordegangsta@gmail.com>
2026-01-31 10:59:20 -0500

    Add Alby Master web files

---
+++ app.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    init_db()
+    # Port is required for hosting services like Render
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
